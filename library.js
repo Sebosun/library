d03d6c3ff1ccfb274e18f81696f2aba949ac0b88
Sebosun <sebos@protonmail.com>
2020-06-09 01:49:44 +0200

    created render() function, which will append table once a new item is added to the library

--- library.js
+++ library.js
@@ -1,20 +1,34 @@
+let tableElem = document.querySelector('#display-table');
+
 let myLibrary = [];
 
 function Book(name, author, pages, read) {
   this.name = name;
   this.author = author;
   this.pages = pages;
-  //read will be a boolean
-  this.read = this.read ? 'read' : 'not read';
-  // the constructor...
+  //read should be a boolean
+  this.read = read ? 'read' : 'not read';
 }
 
 function addBookToLibrary(bookObj) {
   myLibrary.push(bookObj);
+  render(bookObj);
   console.log(`Push of ${bookObj.name} completed`);
-  // do stuff here
+}
+
+function render(book){
+  let content = document.createElement('tr');
+  let bookArrayConversion = Object.values(book);
+  for (let i = 0; i < bookArrayConversion.length; i++){
+    let cell = document.createElement("td");
+    var cellText = document.createTextNode(`${bookArrayConversion[i]}`);
+    cell.appendChild(cellText);
+    content.appendChild(cell);
+  }
+  tableElem.appendChild(content);
 }
 
 let Hobbit = new Book('The Hobbit','Tolkien', 100, false);
 
 addBookToLibrary(Hobbit);
+
