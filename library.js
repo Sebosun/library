c600df32e200b9d2e7cd4f782b054db553ec0485
Sebosun <sebos@protonmail.com>
2020-06-09 00:50:01 +0200

    Book constructur, wrote addBookToLibrary function'

--- library.js
+++ library.js
@@ -1,9 +1,20 @@
 let myLibrary = [];
 
-function Book() {
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  //read will be a boolean
+  this.read = this.read ? 'read' : 'not read';
   // the constructor...
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(bookObj) {
+  myLibrary.push(bookObj);
+  console.log(`Push of ${bookObj.name} completed`);
   // do stuff here
 }
+
+let Hobbit = new Book('The Hobbit','Tolkien', 100, false);
+
+addBookToLibrary(Hobbit);
