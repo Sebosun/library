da7e497d67c15064bbc4d2d2ccef7b41688b4d59
Sebosun <sebos@protonmail.com>
2020-06-16 23:05:16 +0200

    added functioning "delete" button

--- library.js
+++ library.js
@@ -21,14 +21,25 @@ function Book(name, author, pages, read) {
 }
 // function which appends the book to the table)
 function render(book){
+
   let content = document.createElement('tr');
   let bookArrayConversion = Object.values(book);
+
   for (let i = 0; i < bookArrayConversion.length; i++){
     let cell = document.createElement("td");
     var cellText = document.createTextNode(`${bookArrayConversion[i]}`);
     cell.appendChild(cellText);
     content.appendChild(cell);
   }
+  //adds the delete button to each rendered item
+
+  let delElem = document.createElement('td')
+  let delButton = document.createTextNode('X');
+  delElem.className = book.name;
+  delElem.setAttribute('id', 'delete');
+
+  delElem.appendChild(delButton);
+  content.appendChild(delElem);
   tableElem.appendChild(content);
 }
 
@@ -39,10 +50,12 @@ function addBookToLibrary(bookObj) {
   console.log(`Push of ${bookObj.name} completed`);
 }
 
+//hides the mdoal when the "close" button is clicked
 document.querySelector('.close').addEventListener('click', function(){
   document.querySelector('.bg-modal').style.display = 'none';
 });
 
+//reveals the modal when the '+' button is clicked
 document.querySelector('.open').addEventListener('click', function(){
   document.querySelector('.bg-modal').style.display = 'flex';
 });
@@ -52,9 +65,17 @@ document.querySelector('.open').addEventListener('click', function(){
 for (let i = 0; i < localStorageKeys.length; i++){
   render(JSON.parse(localStorage.getItem(localStorageKeys[i])));
 }
-//let Hobbit = new Book('The Hobbit','Tolkien', 100, false);
 
-//addBookToLibrary(Hobbit);
+//selects all buttons with the #delete id and attaches an event listener to each
+//as the delete buttons are generated with the class name of the localStorage key
+//once the key is entered a given key with a given classname is removed from localstorage
+const deleteButtons = document.querySelectorAll('#delete');
+deleteButtons.forEach((button) => {
+  button.addEventListener('click', (e) =>{
+    localStorage.removeItem(button.className);
+    location.reload();
+  })
+})
 
 form.addEventListener('submit', (e) => {
   let newBook = new Book(bookInput.value, authorInput.value, pagesInput.value, document.querySelector('input[name="has-read"]:checked').value)
